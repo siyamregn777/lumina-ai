34d097e2e755f1866fccbd7e6ae0f2dc6c9bc9db
siyamregn777 <yeshidagnasiyamregn@gmail.com>
2026-01-17 22:36:50 +0300

    change trial period

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -50,7 +50,7 @@ const Hero: React.FC = () => {
             </div>
             <div className="flex items-center">
               <CheckCircle2 className="w-5 h-5 text-green-500 mr-2" />
-              14-day free trial
+              15-day free trial
             </div>
             <div className="flex items-center">
               <CheckCircle2 className="w-5 h-5 text-green-500 mr-2" />
